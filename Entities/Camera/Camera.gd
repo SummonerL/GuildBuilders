f6f5b31e5094c4421e0dbaaa8c56eb439be43f5b
Elliot Simpson <simpsonj@uncw.edu>
2020-03-07 23:50:38 -0500

    camera move with cursor

--- Entities/Camera/Camera.gd
+++ Entities/Camera/Camera.gd
@@ -1,10 +1,44 @@
 extends Node2D
 
+const TILE_HEIGHT = 16
+const TILE_WIDTH = 16
+const TILES_PER_ROW = 10
+const TILES_PER_COL = 9
+
+# camera pos on world map
+var pos_x = 0
+var pos_y = 0
+
 func turnOn():
 	get_node("Camera_Node").current = true
 	pass
+	
+func camera_init():
+	# position camera
+	self.global_position = Vector2(pos_x*16, pos_y*16)
+	pass
+	
+func set_camera_pos(new_pos_x, new_pos_y):
+	pos_x = new_pos_x
+	pos_y = new_pos_y
+	
+	self.global_position = Vector2(pos_x*TILE_WIDTH, pos_y*TILE_HEIGHT)
 
+func check_pos(curs_x, curs_y):
+	# if cursor is offscreen, move the cam!
+	print(curs_x)
+	print(pos_x)
+	if curs_x < pos_x:
+		set_camera_pos(curs_x, pos_y)
+	if curs_x > (pos_x + (TILES_PER_ROW-1)):
+		set_camera_pos(pos_x+1, pos_y)
+	if curs_y < pos_y:
+		set_camera_pos(pos_x, curs_y)
+	if curs_y > (pos_y + (TILES_PER_COL-1)):
+		set_camera_pos(pos_x, pos_y+1)
+		
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	camera_init()
 	pass # Replace with function body.
 
--- Entities/Player/Cursor.gd
+++ Entities/Player/Cursor.gd
@@ -7,10 +7,15 @@ const TILE_WIDTH = 16
 var pos_x = 4
 var pos_y = 4
 
+# child nodes
+onready var move_sound = get_node("Cursor_Move_Sound")
+
+# instanced nodes
+onready var camera = get_tree().get_nodes_in_group("Camera")[0]
+
 func cursor_init():
 	# position cursor
 	self.global_position = Vector2(pos_x*16, pos_y*16)
-	pass
 	
 func set_cursor_pos(new_pos_x, new_pos_y):
 	pos_x = new_pos_x
@@ -18,20 +23,36 @@ func set_cursor_pos(new_pos_x, new_pos_y):
 	
 	self.global_position = Vector2(pos_x*TILE_WIDTH, pos_y*TILE_HEIGHT)
 
+	# update the camera, if necessary
+	camera.check_pos(pos_x, pos_y)
+	
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	cursor_init()
 	pass # Replace with function body.
 
 func _input(event):
+	var cursor_move = false
+	
 	if event.is_action_pressed("ui_left"):
 		set_cursor_pos(pos_x-1, pos_y)
+		cursor_move = true
 	if event.is_action_pressed("ui_right"):
 		set_cursor_pos(pos_x+1, pos_y)
+		cursor_move = true
 	if event.is_action_pressed("ui_up"):
 		set_cursor_pos(pos_x, pos_y-1)
+		cursor_move = true
 	if event.is_action_pressed("ui_down"):
 		set_cursor_pos(pos_x, pos_y+1)
+		cursor_move = true
+		
+	if cursor_move:
+		# play cursor move sound
+		move_sound.play()
+		
+		# update camera pos
+		
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 # func _process(delta):
