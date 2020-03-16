7aed75f91f665b4aff8dd5b45bfb741a4fdd6802
SummonerL <elliotsminion@gmail.com>
2020-03-15 22:04:23 -0400

    cleanup

--- Entities/Player/Units/Unit_Class.gd
+++ Entities/Player/Units/Unit_Class.gd
@@ -17,8 +17,8 @@ onready var tileset_props_l2 = get_tree().get_nodes_in_group(constants.MAP_TILES
 # keep track of the eligible tiles the unit can currently move to
 var eligible_movement_tiles = []
 
-# create a  set of tiles that we are iterating through, to track the current distance
-var s_tiles = {}
+# this dict will be used to track the eligible movement tiles, as well as their total distance
+var eligible_tile_tracker = {}
 
 # the unit's position on the map
 var unit_pos_x = 0
@@ -29,12 +29,6 @@ var base_move = 0
 
 # the unit's name
 var unit_name = ""
-
-# for the spiral algorithm
-enum direction {
-	CLOCKWISE
-	COUNTERCLOCKWISE
-}
 
 # set unit position
 func set_unit_pos(target_x, target_y):
@@ -85,26 +79,14 @@ func calculate_eligible_tiles():
 	# clear out the eligible movement tiles
 	eligible_movement_tiles.clear()
 	
-	# clear our s_tiles tracker (for visited tiles)
-	s_tiles = {}
+	# clear our eligible_tile_tracker (as we are calculating a new set of tiles)
+	eligible_tile_tracker = {}
 	
-	# calculate the distance, using the flood fill algorithm
+	# calculate the eligible tiles + distance, using the flood fill algorithm
 	flood_fill(unit_pos_x, unit_pos_y, base_move, {})
 	
-#	spiral(s_tiles, direction.COUNTERCLOCKWISE)
-#	spiral(s_tiles, direction.CLOCKWISE)
-#	spiral(s_tiles, direction.COUNTERCLOCKWISE)
-#	spiral(s_tiles, direction.CLOCKWISE)
+	print(eligible_tile_tracker)
 	
-	# we've calculated the distance of all tiles, now let's determine if they
-	# are eligible for movement, or not
-#	for tile in s_tiles:
-#		var tileObj = s_tiles[tile]
-#		# if the distance is less than the unit's movement, and we're not already there
-#		if (tileObj.distance > 0 && tileObj.distance <= base_move): 
-#			eligible_movement_tiles.push_back(Vector2(tileObj.pos_x, tileObj.pos_y))
-#			show_movement_grid_square(tileObj.pos_x, tileObj.pos_y)
-
 # should have started with this...
 # flood fill is the best approach for calculating eligible tiles
 # it's a recursive function
@@ -129,9 +111,9 @@ func flood_fill(foc_x, foc_y, remaining_move, visited_tiles):
 	var tile_name_l1 = tileset_props_l1.get_tile_at_coordinates(Vector2(foc_x, foc_y))
 	var tile_name_l2 = tileset_props_l2.get_tile_at_coordinates(Vector2(foc_x, foc_y))
 
-	# usually, movement cost is 1. Some tiles may take more movement to move on to
-	# to determine this, we add the movement costs of both layers
-	# start by setting it to the distance of the last tile
+	# usually, tiles cost 1 movement. Some tiles may take more movement to move on to.
+	# to determine this, we add the movement costs of both layers and subtract it from
+	# the remaining movement the unit has.
 	
 	# subtract the cost of the l1 tiles (if they exist. Else, we DEFINITELY can't move there!)
 	if (tile_name_l1 != null):
@@ -145,29 +127,18 @@ func flood_fill(foc_x, foc_y, remaining_move, visited_tiles):
 		
 	# the tile is eligible if the remaining_move is >= 0
 	if (foc_x == unit_pos_x && foc_y == unit_pos_y):
-		remaining_move = base_move # we're still at the base
+		remaining_move = base_move # we're still at the origin tile
 	else:
 		if (remaining_move >= 0):
 			eligible_movement_tiles.push_back(Vector2(foc_x, foc_y))
 			show_movement_grid_square(foc_x, foc_y)
-
-	print('FOC X ' + String(foc_x))
-	print('FOC_Y ' + String(foc_y))
-	print('REMAINING MOVE ' + String(remaining_move))
-	print(" ")
 	
+	# whether or not we need to flood out from this tile
 	var initiate_recursion = true
 	
-	# if we've already been here in this recursion chain, skip (otherwise, StackOverflow)
+	# if we've already been here in this recursion chain, don't recurse (otherwise, StackOverflow)
 	if (visited_tiles.get(String(foc_x) + "_" + String(foc_y)) != null):
-		if (foc_x == unit_pos_x && foc_y == unit_pos_y):
-			pass
-		else:
-			print("SKIPPING")
-			print(String(foc_x) + "_" + String(foc_y))
-			print(visited_tiles.get(String(foc_x) + "_" + String(foc_y)))
-			print(" ")
-			
+		if (foc_x != unit_pos_x || foc_y != unit_pos_y):
 			initiate_recursion = false
 	
 	# mark the tile as visited
@@ -176,12 +147,23 @@ func flood_fill(foc_x, foc_y, remaining_move, visited_tiles):
 		visited_tiles[String(foc_x) + "_" + String(foc_y)] = true
 
 	# mark the distance of the tile
-	if (s_tiles.get(String(foc_x) + "_" + String(foc_y)) == null):
-		s_tiles[String(foc_x) + "_" + String(foc_y)] = base_move - remaining_move
+	if (eligible_tile_tracker.get(String(foc_x) + "_" + String(foc_y)) == null):
+		eligible_tile_tracker[String(foc_x) + "_" + String(foc_y)] = {
+			"pos_x": foc_x,
+			"pos_y": foc_y,
+			"distance": base_move - remaining_move
+		}
 	else:
-		if (s_tiles.get(String(foc_x) + "_" + String(foc_y)) > base_move - remaining_move):
-			# we've got a cheaper path to get here!
-			s_tiles[String(foc_x) + "_" + String(foc_y)] = base_move - remaining_move
+		if (eligible_tile_tracker.get(String(foc_x) + "_" + String(foc_y)).distance > base_move - remaining_move):
+			# we've got a cheaper path to get to this tile!
+			eligible_tile_tracker[String(foc_x) + "_" + String(foc_y)] = {
+				"pos_x": foc_x,
+				"pos_y": foc_y,
+				"distance": base_move - remaining_move
+			}
+			
+			# we want to recurse from here, to make sure adjacent tiles are updated
+			# with a cheaper distance
 			initiate_recursion = true
 
 	if (initiate_recursion):
@@ -189,206 +171,3 @@ func flood_fill(foc_x, foc_y, remaining_move, visited_tiles):
 		flood_fill(foc_x - 1, foc_y, remaining_move, visited_tiles); # tile to the west
 		flood_fill(foc_x, foc_y + 1, remaining_move, visited_tiles); # tile to the south
 		flood_fill(foc_x, foc_y - 1, remaining_move, visited_tiles); # tile to the north
-
-
-# I can't claim to have come up with this 'Spiral' algorithm...
-# tbh, I grabbed it off of StackOverflow
-func spiral(s_tiles, dir):
-	# width and height of the range of tiles
-	# i.e move 1 = 3x3, move 2 = 5x5
-	var sq_size = (base_move*2) + 1 
-	
-	# current focus. Will start at the "origin", but will be shifted
-	# to the actual world map coordinates
-	var foc_x = 0 
-	var foc_y = 0
-	
-	# keep track of all the tiles we're iterating over, as well as their distance
-	# to the unit
-
-	s_tiles[String(foc_x) + "_" + String(foc_y)] = {
-		"pos_x": foc_x + unit_pos_x,
-		"pos_y": foc_y + unit_pos_y,
-		"distance": 0
-	} # the starting tile, distance of 0
-
-	
-	var change_x
-	if (dir == direction.CLOCKWISE):
-		change_x = 0
-	else:
-		change_x = -1
-
-	var change_y
-	if (dir == direction.CLOCKWISE):
-		change_y = -1
-	else:
-		change_y = 0
-	
-	for _i in range(pow(sq_size,2)):
-		# calculate the distance, based on the tile's neighbors
-		var shortest_distance = constants.CANT_MOVE # by default, assume we can't move to the tile
-		
-		if (s_tiles.get(String(foc_x+1) + "_" + String(foc_y)) != null): # tile to the right
-			var neighbor_right_dist = s_tiles.get(String(foc_x+1) + "_" + String(foc_y)).distance
-			if (neighbor_right_dist < shortest_distance):
-				shortest_distance = neighbor_right_dist
-			
-		if (s_tiles.get(String(foc_x-1) + "_" + String(foc_y)) != null): # tile to the left
-			var neighbor_left_dist = s_tiles.get(String(foc_x-1) + "_" + String(foc_y)).distance
-			if (neighbor_left_dist < shortest_distance):
-				shortest_distance = neighbor_left_dist
-				
-		if (s_tiles.get(String(foc_x) + "_" + String(foc_y-1)) != null): # tile to the north
-			var neighbor_up_dist = s_tiles.get(String(foc_x) + "_" + String(foc_y-1)).distance
-			if (neighbor_up_dist < shortest_distance):
-				shortest_distance = neighbor_up_dist
-			
-		if (s_tiles.get(String(foc_x) + "_" + String(foc_y+1)) != null): # tile to the south
-			var neighbor_down_dist = s_tiles.get(String(foc_x) + "_" + String(foc_y+1)).distance
-			if (neighbor_down_dist < shortest_distance):
-				shortest_distance = neighbor_down_dist
-		
-		var tile_name_l1 = tileset_props_l1.get_tile_at_coordinates(Vector2(foc_x + unit_pos_x, foc_y + unit_pos_y))
-		var tile_name_l2 = tileset_props_l2.get_tile_at_coordinates(Vector2(foc_x + unit_pos_x, foc_y + unit_pos_y))
-		
-		# usually, movement cost is 1. Some tiles may take more movement to move on to
-		var new_tile_distance = shortest_distance
-		
-		# add the cost of the l1 tiles (if they exist. Else, we DEFINITELY can't move there!)
-		if (tile_name_l1 != null):
-			new_tile_distance += tileset_props_l1.get_movement_cost(tile_name_l1)
-		else:
-			new_tile_distance = constants.CANT_MOVE
-			
-		# add the cost of the l2 tiles, if they exist
-		if (tile_name_l2 != null):
-			new_tile_distance += tileset_props_l2.get_movement_cost(tile_name_l2)
-		
-		# update the distance of this particular tile
-		if (!(foc_x == 0 && foc_y == 0)):
-			s_tiles[String(foc_x) + "_" + String(foc_y)] = {
-				"pos_x": foc_x + unit_pos_x,
-				"pos_y": foc_y + unit_pos_y,
-				"distance": new_tile_distance
-			}
-		else:
-			new_tile_distance = 0
-		
-		# now, we need to update the distance of all adjacent neighbors
-		
-		# neighbor right
-		var tile_name_right_l1 = tileset_props_l1.get_tile_at_coordinates(Vector2(foc_x + unit_pos_x + 1, foc_y + unit_pos_y))
-		var tile_name_right_l2 = tileset_props_l2.get_tile_at_coordinates(Vector2(foc_x + unit_pos_x + 1, foc_y + unit_pos_y))
-		var tile_right_move_cost = 0
-		
-		if (tile_name_right_l1 != null):
-			tile_right_move_cost += tileset_props_l1.get_movement_cost(tile_name_right_l1)
-		else:
-			tile_right_move_cost += constants.CANT_MOVE
-		if (tile_name_right_l2 != null):
-			tile_right_move_cost += tileset_props_l1.get_movement_cost(tile_name_right_l1)
-
-		var current_tile_right_distance = constants.CANT_MOVE
-		
-		if (s_tiles.get(String(foc_x+1) + "_" + String(foc_y)) != null):
-			current_tile_right_distance = s_tiles.get(String(foc_x+1) + "_" + String(foc_y)).distance
-		
-		if (current_tile_right_distance > (new_tile_distance + tile_right_move_cost)):
-			s_tiles[String(foc_x+1) + "_" + String(foc_y)] = {
-				"pos_x": (foc_x+1) + unit_pos_x,
-				"pos_y": foc_y + unit_pos_y,
-				"distance": new_tile_distance + tile_right_move_cost
-			}
-		
-		# neighbor down
-		var tile_name_down_l1 = tileset_props_l1.get_tile_at_coordinates(Vector2(foc_x + unit_pos_x, foc_y + unit_pos_y+1))
-		var tile_name_down_l2 = tileset_props_l2.get_tile_at_coordinates(Vector2(foc_x + unit_pos_x, foc_y + unit_pos_y+1))
-		var tile_down_move_cost = 0
-		
-		if (tile_name_down_l1 != null):
-			tile_down_move_cost += tileset_props_l1.get_movement_cost(tile_name_down_l1)
-		else:
-			tile_down_move_cost += constants.CANT_MOVE
-		if (tile_name_down_l2 != null):
-			tile_down_move_cost += tileset_props_l1.get_movement_cost(tile_name_down_l1)
-
-		var current_tile_down_distance = constants.CANT_MOVE
-		
-		if (s_tiles.get(String(foc_x) + "_" + String(foc_y+1)) != null):
-			current_tile_down_distance = s_tiles.get(String(foc_x) + "_" + String(foc_y+1)).distance
-		
-		if (current_tile_down_distance > (new_tile_distance + tile_down_move_cost)):
-			s_tiles[String(foc_x) + "_" + String(foc_y+1)] = {
-				"pos_x": foc_x + unit_pos_x,
-				"pos_y": (foc_y+1) + unit_pos_y,
-				"distance": new_tile_distance + tile_down_move_cost
-			}
-			
-
-		# neighbor left
-		var tile_name_left_l1 = tileset_props_l1.get_tile_at_coordinates(Vector2(foc_x + unit_pos_x - 1, foc_y + unit_pos_y))
-		var tile_name_left_l2 = tileset_props_l2.get_tile_at_coordinates(Vector2(foc_x + unit_pos_x - 1, foc_y + unit_pos_y))
-		var tile_left_move_cost = 0
-		
-		if (tile_name_left_l1 != null):
-			tile_left_move_cost += tileset_props_l1.get_movement_cost(tile_name_left_l1)
-		else:
-			tile_left_move_cost += constants.CANT_MOVE
-		if (tile_name_left_l2 != null):
-			tile_left_move_cost += tileset_props_l1.get_movement_cost(tile_name_left_l1)
-
-		var current_tile_left_distance = constants.CANT_MOVE
-		
-		if (s_tiles.get(String(foc_x-1) + "_" + String(foc_y)) != null):
-			current_tile_left_distance = s_tiles.get(String(foc_x-1) + "_" + String(foc_y)).distance
-		
-		if (current_tile_left_distance > (new_tile_distance + tile_left_move_cost)):
-			s_tiles[String(foc_x-1) + "_" + String(foc_y)] = {
-				"pos_x": (foc_x-1) + unit_pos_x,
-				"pos_y": foc_y + unit_pos_y,
-				"distance": new_tile_distance + tile_left_move_cost
-			}
-			
-		# neighbor up
-		var tile_name_up_l1 = tileset_props_l1.get_tile_at_coordinates(Vector2(foc_x + unit_pos_x, foc_y + unit_pos_y-1))
-		var tile_name_up_l2 = tileset_props_l2.get_tile_at_coordinates(Vector2(foc_x + unit_pos_x, foc_y + unit_pos_y-1))
-		var tile_up_move_cost = 0
-		
-		if (tile_name_up_l1 != null):
-			tile_up_move_cost += tileset_props_l1.get_movement_cost(tile_name_up_l1)
-		else:
-			tile_up_move_cost += constants.CANT_MOVE
-		if (tile_name_up_l2 != null):
-			tile_up_move_cost += tileset_props_l1.get_movement_cost(tile_name_up_l1)
-
-		var current_tile_up_distance = constants.CANT_MOVE
-		
-		if (s_tiles.get(String(foc_x) + "_" + String(foc_y-1)) != null):
-			current_tile_up_distance = s_tiles.get(String(foc_x) + "_" + String(foc_y-1)).distance
-		
-		if (current_tile_up_distance > (new_tile_distance + tile_up_move_cost)):
-			s_tiles[String(foc_x) + "_" + String(foc_y-1)] = {
-				"pos_x": foc_x + unit_pos_x,
-				"pos_y": (foc_y-1) + unit_pos_y,
-				"distance": new_tile_distance + tile_up_move_cost
-			}
-
-		if (dir == direction.CLOCKWISE):
-			if ((foc_x == foc_y) ||
-				(foc_x < 0 && foc_x == -foc_y) ||
-				(foc_x > 0 && foc_x == 1-foc_y)):
-				var temp_c_x = change_x
-				var temp_c_y = change_y
-				change_x = -temp_c_y
-				change_y = temp_c_x
-		else:
-			if ((foc_x == foc_y) ||
-				(foc_y < 0 && foc_y == -foc_x) ||
-				(foc_y > 0 && foc_y == 1-foc_x)):
-				var temp_c_x = change_x
-				var temp_c_y = change_y
-				change_x = temp_c_y
-				change_y = -temp_c_x
-		foc_x += change_x
-		foc_y += change_y
