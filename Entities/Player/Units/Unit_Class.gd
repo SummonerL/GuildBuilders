22de1d15b222e440f21207d51e6e312f921a1490
SummonerL <elliotsminion@gmail.com>
2020-03-17 08:21:12 -0400

    a star pathfinding

--- Entities/Player/Units/Unit_Class.gd
+++ Entities/Player/Units/Unit_Class.gd
@@ -114,16 +114,21 @@ func flood_fill(foc_x, foc_y, remaining_move, visited_tiles):
 	# usually, tiles cost 1 movement. Some tiles may take more movement to move on to.
 	# to determine this, we add the movement costs of both layers and subtract it from
 	# the remaining movement the unit has.
+	var total_tile_cost = 0 # we'll need this later for a*
 	
 	# subtract the cost of the l1 tiles (if they exist. Else, we DEFINITELY can't move there!)
 	if (tile_name_l1 != null):
-		remaining_move -= tileset_props_l1.get_movement_cost(tile_name_l1)
+		var l1_cost = tileset_props_l1.get_movement_cost(tile_name_l1)
+		remaining_move -= l1_cost
+		total_tile_cost += l1_cost
 	else:
 		remaining_move -= constants.CANT_MOVE
 	
 	# subtract the cost of the l2 tiles, if they exist
 	if (tile_name_l2 != null):
-		remaining_move -= tileset_props_l2.get_movement_cost(tile_name_l2)
+		var l2_cost = tileset_props_l2.get_movement_cost(tile_name_l2)
+		remaining_move -= l2_cost
+		total_tile_cost += l2_cost
 		
 	# the tile is eligible if the remaining_move is >= 0
 	if (foc_x == unit_pos_x && foc_y == unit_pos_y):
@@ -151,7 +156,8 @@ func flood_fill(foc_x, foc_y, remaining_move, visited_tiles):
 		eligible_tile_tracker[String(foc_x) + "_" + String(foc_y)] = {
 			"pos_x": foc_x,
 			"pos_y": foc_y,
-			"distance": base_move - remaining_move
+			"distance": base_move - remaining_move,
+			"tile_cost": total_tile_cost
 		}
 	else:
 		if (eligible_tile_tracker.get(String(foc_x) + "_" + String(foc_y)).distance > base_move - remaining_move):
@@ -159,7 +165,8 @@ func flood_fill(foc_x, foc_y, remaining_move, visited_tiles):
 			eligible_tile_tracker[String(foc_x) + "_" + String(foc_y)] = {
 				"pos_x": foc_x,
 				"pos_y": foc_y,
-				"distance": base_move - remaining_move
+				"distance": base_move - remaining_move,
+				"tile_cost": total_tile_cost
 			}
 			
 			# we want to recurse from here, to make sure adjacent tiles are updated
@@ -185,7 +192,11 @@ func a_star(target_x, target_y):
 	var path = []
 	
 	var start_tile = eligible_tile_tracker.get(String(unit_pos_x) + "_" + String(unit_pos_y))
-
+	
+	eligible_tile_tracker[String(target_x) + "_" + String(target_y)].g = 0
+	eligible_tile_tracker[String(target_x) + "_" + String(target_y)].h = 0
+	eligible_tile_tracker[String(target_x) + "_" + String(target_y)].f = 0
+	
 	# g = the distance between the current node and the start node
 	# h = heuristic - the estimated distance from this node to the target
 	# f = g + h
@@ -195,7 +206,7 @@ func a_star(target_x, target_y):
 	start_tile.f = 0 # for the start tile, leave f at 0
 		
 	open_list.push_front(start_tile)
-	
+
 	while open_list.size() > 0:
 		var cheapest_tile = open_list[0]
 		# god, why can't gdscript keep track of the index
@@ -203,12 +214,14 @@ func a_star(target_x, target_y):
 		var index = 0
 		
 		# grab the tile with the lowest f value
+		#print("OPEN LIST")
+		#print(open_list)
+		#print(" ")
 		for tile in open_list:
 			if tile.f < cheapest_tile.f:
 				cheapest_tile = tile
 				cheapest_tile_index = index
 			index+=1
-			
 		
 		# remove the tile from the open_list, and add to the close_list
 		open_list.remove(cheapest_tile_index)
@@ -269,13 +282,25 @@ func a_star(target_x, target_y):
 			if (hasChild):
 				continue
 				
-			# create the f, g, and h values
-			child.g = child.distance
-			child.h = abs(target_x - child.pos_x) + abs(target_y - child.pos_y)
-			child.f = child.g + child.h
-			
+			# make sure that the unit can actually walk here
+			if (child.tile_cost > base_move):
+				continue
+				
 			# keep track of the parent
 			child.parent = cheapest_tile
+			
+			# create the f, g, and h values, unless the child is the target
+			# in that case, leave the f cost as 0
+			if (child.pos_x != target_x || child.pos_y != target_y):
+				child.g = child.parent.g + child.tile_cost
+				# experiment with different heuristics for more interesting
+				# pathfinding
+				child.h = abs(child.pos_x - target_x) + abs(child.pos_y + target_y) + child.tile_cost
+				#child.h = pow(child.pos_x - target_x, 2) + pow(child.pos_y - target_y, 2)
+				child.f = child.g + child.h
+			
+			
+
 			
 			# if the child is already in the open list,
 			# and the current child's g cost is higher than
